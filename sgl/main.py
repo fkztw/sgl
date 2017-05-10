1b979795fb66a90859bf063edb38f242c87cc8b6
shunyi <M157q.tw@gmail.com>
2017-05-11 03:45:50 +0800

    Use API instead of parsing the website.

--- sgl/main.py
+++ sgl/main.py
@@ -1,54 +1,74 @@
 #!/usr/bin/env python3
 
-import re
+import json
 import time
 
 import requests
-import bs4
 
 from logger import logger
 
 
-url = 'https://rent.591.com.tw/new/?kind=1&region=1&rentprice=0,26000&patternMore=2&option=cold&hasimg=1&not_cover=1'
-link_pattern = re.compile('//rent.591.com.*')
+API = "https://rent.591.com.tw/home/search/rsList"
+CONDITIONS = {
+    'is_new_list': '1',
+    'type': '1',
+    'kind': '1',
+    'searchtype': '1',
+    'region': '1',
+    'rentprice': '0,26000',
+    'patternMore': '2',
+    'option': 'cold',
+    'hasimg': '1',
+    'not_cover': '1',
+}
+WEB_URL_FORMAT_STR = "https://rent.591.com.tw/rent-detail-{}.html"
+
 cache = set()
 
 
-def content_clean_up(string):
-    return ''.join(map(str.strip, string.split()))
-
-
 def get_houses():
-    logger.info('requests 591 ...')
-    response = requests.get(url)
-    html = bs4.BeautifulSoup(response.text, 'html.parser')
-    houses = tuple(
-        filter(
-            lambda x: x and x != '\n',
-            html.find(id='content').children
+    logger.info('requests 591 API...')
+    response = requests.get(API, params=CONDITIONS)
+    response_json = json.loads(response.text)
+
+    try:
+        data = response_json['data']
+    except KeyError:
+        logger.error("Cannnot get data from response_json")
+    except:
+        raise
+    else:
+        houses = data.get('data', [])
+        logger.info(len(houses))
+
+        for house in reversed(houses):
+            yield house
+
+
+def log_house_info(house):
+    logger.info(
+        "名稱：{}-{}-{}".format(
+            house['region_name'],
+            house['section_name'],
+            house['fulladdress'],
         )
     )
-    links = map(lambda x: x.find(href=link_pattern), houses)
-    links = map(lambda x: 'https:' + x.get('href').strip(), links)
-    descriptions = map(
-        lambda x: ''.join(
-            map(
-                lambda s: content_clean_up(s.text),
-                x.find_all(class_="lightBox")
-            )
-        ),
-        houses
-    )
-    return tuple(zip(links, descriptions))
+    logger.info("網址：{}".format(WEB_URL_FORMAT_STR.format(house['post_id'])))
+    logger.info("租金：{} {}".format(house['price'], house['unit']))
+    logger.info("坪數：{} 坪".format(house['area']))
+    logger.info("格局：{}".format(house['layout']))
+    logger.info("更新時間：{}".format(time.ctime(house['refreshtime'])))
+    logger.info("=" * 80)
 
 
 def search_houses():
     houses = get_houses()
-    for h in houses:
-        if h in cache:
+    for house in houses:
+        if house['post_id'] in cache:
             break
-        logger.info('new house: {}'.format(h))
-    cache.update(houses)
+
+        log_house_info(house)
+        cache.update([house['post_id']])
 
 
 def main():
