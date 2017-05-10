96f64ba7729c9d36ae54386ecb84c0154588c8a1
shunyi <M157q.tw@gmail.com>
2017-05-11 01:03:24 +0800

    Merge update from origin gist
    <https://gist.github.com/wdv4758h/e85c0e4a9ead1f55272df88edff6c5ce/revisions?diff=split#diff-87fd89be4ca20dffcc74a91ba8b09596>

--- sgl/main.py
+++ sgl/main.py
@@ -9,23 +9,41 @@ import bs4
 from logger import logger
 
 
-url = 'https://rent.591.com.tw/new/?kind=1&region=1&rentprice=0,26000&patternMore=2&option=cold&hasimg=1&not_cover=1&order=posttime&orderType=desc'
+url = 'https://rent.591.com.tw/new/?kind=1&region=1&rentprice=0,26000&patternMore=2&option=cold&hasimg=1&not_cover=1'
 link_pattern = re.compile('//rent.591.com.*')
 cache = set()
 
 
-def get_links():
+def content_clean_up(string):
+    return ''.join(map(str.strip, string.split()))
+
+
+def get_houses():
     logger.info('requests 591 ...')
     response = requests.get(url)
     html = bs4.BeautifulSoup(response.text, 'html.parser')
-    houses = filter(lambda x: x and x != '\n', html.find(id='content').children)
-    houses = map(lambda x: x.find(href=link_pattern), houses)
-    houses = map(lambda x: 'https:' + x.get('href').strip(), houses)
-    return tuple(houses)
+    houses = tuple(
+        filter(
+            lambda x: x and x != '\n',
+            html.find(id='content').children
+        )
+    )
+    links = map(lambda x: x.find(href=link_pattern), houses)
+    links = map(lambda x: 'https:' + x.get('href').strip(), links)
+    descriptions = map(
+        lambda x: ''.join(
+            map(
+                lambda s: content_clean_up(s.text),
+                x.find_all(class_="lightBox")
+            )
+        ),
+        houses
+    )
+    return tuple(zip(links, descriptions))
 
 
 def search_houses():
-    houses = get_links()
+    houses = get_houses()
     for h in houses:
         if h in cache:
             break
@@ -36,7 +54,7 @@ def search_houses():
 def main():
     while True:
         search_houses()
-        time.sleep(10)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
