f02019b17a78ef86917add3de21bc2ee395cbb65
shunyi <M157q.tw@gmail.com>
2018-06-23 15:52:23 +0800

    Add index.html and map.html

--- sgl/endpoints.py
+++ sgl/endpoints.py
@@ -1,8 +1,38 @@
-from flask import Blueprint, Response
+from flask import Blueprint, render_template, current_app, request
+from flask_googlemaps import Map
+
 
 BP = Blueprint('root', __name__)
 
 
 @BP.route("/", methods=["GET"])
 def index():
-    return Response()
+    return render_template(
+        'index.html',
+        title=current_app.config.get('INDEX_PAGE_TITLE'),
+    )
+
+@BP.route("/map", methods=["GET"])
+def map():
+    # TODO
+    # payload = request.json or request.form
+    # Get rental locations
+
+    # TODO
+    # Add all locations's lat and lng into markers
+    # markers =
+
+    google_map = Map(
+        identifier='sgl_map',
+        lat=25.044955,
+        lng=121.335579,
+        style='height:80%;width:80%;margin:1%;',
+        language='zh-tw',
+        region='TW',
+    )
+
+    return render_template(
+        'map.html',
+        title=current_app.config.get('MAP_PAGE_TITLE'),
+        google_map=google_map,
+    )
--- sgl/settings/default.py
+++ sgl/settings/default.py
@@ -4,4 +4,6 @@ import os
 GOOGLEMAPS_KEY = os.environ.get('GOOGLEMAPS_KEY')
 
 # sgl settings
+INDEX_PAGE_TITLE = os.environ.get('INDEX_PAGE_TITLE')
+MAP_PAGE_TITLE = os.environ.get('MAP_PAGE_TITLE')
 PARSE_INTERVAL_IN_SECONDS = os.environ.get('PARSE_INTERVAL_IN_SECONDS') or 1800
