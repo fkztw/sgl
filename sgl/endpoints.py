62afbfb53dda2d6a5abae1c26bfdc181956b005c
shunyi <M157q.tw@gmail.com>
2018-06-24 03:05:06 +0800

    Add form to submit

--- sgl/endpoints.py
+++ sgl/endpoints.py
@@ -1,5 +1,12 @@
-from flask import Blueprint, render_template, current_app, request
+from flask import (
+    Blueprint,
+    render_template,
+    current_app,
+    request,
+)
 from flask_googlemaps import Map
+
+from sgl.forms import QueryForm
 
 
 BP = Blueprint('root', __name__)
@@ -7,25 +14,35 @@ BP = Blueprint('root', __name__)
 
 @BP.route("/", methods=["GET"])
 def index():
+    form = QueryForm()
+
     return render_template(
         'index.html',
         title=current_app.config.get('INDEX_PAGE_TITLE'),
+        form=form,
     )
 
-@BP.route("/map", methods=["GET"])
-def map():
+
+@BP.route("/map", methods=["POST"])
+def query_map():
     # TODO
-    # payload = request.json or request.form
+    payload = request.json or request.form
+    print(payload)
     # Get rental locations
 
     # TODO
     # Add all locations's lat and lng into markers
-    # markers =
+    markers = None
+
+    # TODO
+    # Get centered object lat and lng
+    lat, lng = 25.04, 121.32
 
     google_map = Map(
         identifier='sgl_map',
-        lat=25.044955,
-        lng=121.335579,
+        lat=lat,
+        lng=lng,
+        markers=markers,
         style='height:80%;width:80%;margin:1%;',
         language='zh-tw',
         region='TW',
--- sgl/settings/default.py
+++ sgl/settings/default.py
@@ -3,7 +3,12 @@ import os
 # Flask-GoogleMaps settings
 GOOGLEMAPS_KEY = os.environ.get('GOOGLEMAPS_KEY')
 
+# Flask-WTF settings
+WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
+
+# Flask settings
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # sgl settings
 INDEX_PAGE_TITLE = os.environ.get('INDEX_PAGE_TITLE')
 MAP_PAGE_TITLE = os.environ.get('MAP_PAGE_TITLE')
-PARSE_INTERVAL_IN_SECONDS = os.environ.get('PARSE_INTERVAL_IN_SECONDS') or 1800
