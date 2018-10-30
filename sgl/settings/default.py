bd82c933f1670c87165173f4caf1e3de5d5f46c7
Frank Jheng <M157q.tw@gmail.com>
2018-10-31 04:41:03 +0800

    Add default settings for configs

--- sgl/settings/default.py
+++ sgl/settings/default.py
@@ -4,10 +4,10 @@ import os
 GOOGLEMAPS_KEY = os.environ.get('GOOGLEMAPS_KEY')
 
 # Flask-WTF settings
-WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
+WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY') or os.urandom(32)
 
 # Flask settings
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
 
 # sgl settings
-PAGE_TITLE = os.environ.get('PAGE_TITLE')
+PAGE_TITLE = os.environ.get('PAGE_TITLE') or "sgl"
